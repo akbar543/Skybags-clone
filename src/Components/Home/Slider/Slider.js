adaf72a362f05034f127adfcb138b33c813a1e9a
Anil Choudhary <134296442+AnilChoudhary1151@users.noreply.github.com>
2023-07-26 15:25:52 +0530

    add folder

--- src/Components/Home/Slider/Slider.js
+++ src/Components/Home/Slider/Slider.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Slider = () => {
   return (
     <div>
-      home
+      
     </div>
   )
 }
 
-export default Home
+export default Slider
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Slider from '../../Components/Home/Slider/Slider.js';
+
+const Home = () => {
+  return (
+    <>
+    <Slider/>
+    </>
+  )
+}
+
+export default Home
