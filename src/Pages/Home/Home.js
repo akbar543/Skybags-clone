a4b1dab25d77f75fea47662db24ff8c611d06c07
Anil Choudhary <134296442+AnilChoudhary1151@users.noreply.github.com>
2023-07-28 02:59:14 +0530

    toppicks

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,6 +6,7 @@ import Banner2 from '../../Components/Home/Banner/Banner2.js';
 import Header from '../../Components/Home/Header/Header.js';
 import TopPick from '../../Components/Home/TopPicks/TopPick.js';
 import Feature2 from '../../Components/Home/Features/Feature2.js';
+import data1 from '../../Assets/Data.json';
 
 const Home = () => {
   return (
@@ -13,16 +14,16 @@ const Home = () => {
     <Slider/>
     <span className='topPick'>TopPicks</span>
     <Header item={`Trending What's New`}/>
-    <TopPick/>
+    <TopPick data={data1.topickstrending} />
 
     <Header item={`Top Backpack`}/>
-    <TopPick/>
+    <TopPick data={data1.topickstrending} FeatureData={data1.featurestopicksBackpacks}/>
 
     <Header item={`Top Luggage`}/>
-    <TopPick/>
+    <TopPick data={data1.topickstrending} FeatureData={data1.featurestopicksBackpacks}/>
 
     <Header item={`Top Duffle`}/>
-    <TopPick/>
+    <TopPick data={data1.topickstrending} FeatureData={data1.featurestopicksBackpacks}/>
 
     <Header item={`Occasion`}/>
     <Banner/>
