d89059deb10d2fb2a48a8883774d7e5e9e1550e2
Akbar Khan <akbar29052@gmail.com>
2023-07-28 12:04:52 +0530

    cart-redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
+import FirstPage from './redux/FirstPage';
+import CartItems from './redux/CartItems';
+import CartPage from './redux/CartPage';
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          {/* <Route path='/' element={<Home/>}/> */}
         </Routes>
+          <FirstPage/>
+          <CartItems/>
+          <CartPage/>
       </Router>
     </div>
   );
