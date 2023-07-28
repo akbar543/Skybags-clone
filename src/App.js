f56f54bc4549300015e93fbc7bfc6d4c99c4fd78
Niyati Soni <niya.legend31@gmail.com>
2023-07-28 13:21:15 +0530

    one api changed for image links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Item from './Pages/Item/Item';
 import Footer from './Components/Footer'
+import Temp from './API/temp';
 
 function App() {
 return (
@@ -11,6 +12,7 @@ return (
 <Routes>
 <Route path='/' element={<Home/>}/>
 <Route path='/item' element={<Item/>}/>
+<Route path='/temp' element={<Temp/>}/>
 </Routes>
 <Footer/>
 </Router>
