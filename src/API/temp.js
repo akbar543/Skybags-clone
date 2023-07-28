e6f0eae9611babf177b61b46307f7fcbdb24f4cd
Niyati Soni <niya.legend31@gmail.com>
2023-07-28 13:23:09 +0530

    remove

--- src/API/temp.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Temp() {
-    return ( 
-        <img src="https://skybags.co.in/cdn/shop/files/1_cb3df65e-2913-4957-8650-a78d8f81a58f_900x.jpg?v=1685093839"/>
-     );
-}
-
-export default Temp;
